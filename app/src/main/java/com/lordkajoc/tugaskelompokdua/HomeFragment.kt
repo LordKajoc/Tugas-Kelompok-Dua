ffef587c49295a1d58a97ce6690619a26a529f1b
LordKajoc <rizkinuriman@upi.edu>
2023-04-09 14:12:13 +0700

    Update UI and NavComponent + Data RecyclerView

--- app/src/main/java/com/lordkajoc/tugaskelompokdua/HomeFragment.kt
+++ app/src/main/java/com/lordkajoc/tugaskelompokdua/HomeFragment.kt
@@ -5,11 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.lordkajoc.tugaskelompokdua.databinding.FragmentHomeBinding
 
-class HomeFragment : Fragment() {
-    private lateinit var binding:FragmentHomeBinding
+class HomeFragment : Fragment(), ListAdapterFilm.OnItemClickListener {
+    private lateinit var binding: FragmentHomeBinding
+    private val list = ArrayList<DataFilm>()
+    private lateinit var adapterFilm: ListAdapterFilm
+    private lateinit var filmViewModel : FilmViewModel
+    private lateinit var rvFilm :RecyclerView
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -20,13 +30,45 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val imageprofile = binding.imageView2
-        imageprofile.setOnClickListener {
-            findNavController().navigate(R.id.action_homeFragment_to_profileFragment)
-        }
-        val nyobapindahdetail = binding.tvJudulhome
-        nyobapindahdetail.setOnClickListener {
-            findNavController().navigate(R.id.action_homeFragment_to_detailFragment)
+        val filmList = getListFilm()
+        adapterFilm = ListAdapterFilm(filmList,this)
+        binding.rvFilm.layoutManager = GridLayoutManager(context,2)
+        binding.rvFilm.adapter = adapterFilm
+
+
+        val imageclick = binding.imageView2
+        imageclick.setOnClickListener {
+            onIconClick()
         }
     }
+
+    private fun getListFilm(): ArrayList<DataFilm> {
+        val dataNama = resources.getStringArray(R.array.data_mobil)
+        val dataPreview = resources.getStringArray(R.array.isi_sementara)
+        val dataFoto = resources.obtainTypedArray(R.array.foto_mobil)
+        val dataDeskripsi = resources.getStringArray(R.array.deskrpsi)
+        val dataSinopsis = resources.getStringArray(R.array.data_sinopsis)
+        val listMobil = ArrayList<DataFilm>()
+        for (i in dataNama.indices) {
+            val mobil = DataFilm(
+                dataNama[i],
+                dataPreview[i],
+                dataFoto.getResourceId(i, -1),
+                dataDeskripsi[i],
+                dataSinopsis[i]
+            )
+            listMobil.add(mobil)
+        }
+        return listMobil
+    }
+
+    override fun onItemClick(nama: String) {
+        val itemclick = HomeFragmentDirections.actionHomeFragmentToDetailFragment()
+        findNavController().navigate(itemclick)
+    }
+
+    override fun onIconClick() {
+        findNavController().navigate(R.id.action_homeFragment_to_profileFragment)
+    }
+
 }
